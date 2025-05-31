8e8b1e2b4b8d55af21e7de8f1ae89502d5355607
firebolt778 <firebolt77800@gmail.com>
2025-06-01 08:27:56 +0900

    Add phone number management features and refactor modals
    - Introduced countryPhoneOptions for improved phone number selection.
    - Added ImportNumberModal for importing phone numbers with validation and API integration.
    - Created SetAgentModal for linking agents to phone numbers, enhancing agent management.
    - Implemented SetAgentConfigModal for customizing agent configurations per phone number.
    - Refactored PhoneNumbers component to integrate new modals and improve state management.
    - Removed deprecated PhoneNumbers page, consolidating functionality into the new structure.

---
+++ src/pages/PhoneNumbers/index.tsx
@@ -0,0 +1,300 @@
+import { SetStateAction, Dispatch, useEffect, useState } from "react";
+import { FaDownload, FaEdit, FaEllipsisV, FaUserAlt } from "react-icons/fa";
+import { toast } from "react-toastify";
+
+import axiosInstance from "../../core/axiosInstance";
+import Content from "../../Layout/Content";
+import Table, { TableCell } from "../../library/Table";
+import { AgentTypeRead } from "../../models/agent";
+import { PhoneTypeRead } from "../../models/phone";
+import { formatDateTime } from "../../utils/helpers";
+import { ImportNumberModal } from "./ImportNumberModal";
+import { SetAgentModal } from "./SetAgentModal";
+import { SetAgentConfigModal } from "./SetAgentConfigModal";
+
+interface ActionProps {
+  phone: PhoneTypeRead;
+  setIsChanged: Dispatch<SetStateAction<boolean>>;
+  setIsOverlayShow: Dispatch<SetStateAction<boolean>>;
+  setIsSetConfigModalOpen: Dispatch<SetStateAction<boolean>>;
+  setSelectedPhoneNumber: Dispatch<SetStateAction<PhoneTypeRead | null>>;
+}
+
+const PhoneAction: React.FC<ActionProps> = ({
+  phone,
+  setIsSetConfigModalOpen,
+  setSelectedPhoneNumber,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        isOpen &&
+        !(event.target as HTMLElement).closest(".agent-action-button")
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isOpen]);
+
+  const handleSetAgentConfig = async () => {
+    setSelectedPhoneNumber(phone);
+    setIsSetConfigModalOpen(true);
+  };
+  const handleDelete = async () => {
+    alert("Feature is coming soon.");
+    // setIsOverlayShow(true);
+    // try {
+    //   await axiosInstance.delete(`/agent/${agent.id}`);
+    //   setIsChanged((prev) => !prev);
+    // } catch (error) {
+    //   console.error(error);
+    //   toast.error(`Failed to delete agent: ${error}`);
+    // } finally {
+    //   setIsOverlayShow(false);
+    // }
+  };
+  const handleTagging = () => {
+    alert("Feature is coming soon.");
+    // setIsOverlayShow(true);
+    // try {
+    //   await axiosInstance.delete(`/agent/${agent.id}`);
+    //   setIsChanged((prev) => !prev);
+    // } catch (error) {
+    //   console.error(error);
+    //   toast.error(`Failed to delete agent: ${error}`);
+    // } finally {
+    //   setIsOverlayShow(false);
+    // }
+  };
+
+  return (
+    <>
+      <div className="ml-auto mr-0 relative w-fit">
+        <button
+          className="cursor-pointer hover:bg-gray-700 p-2 rounded-md transition-all duration-300 agent-action-button"
+          onClick={() => setIsOpen(true)}
+        >
+          <FaEllipsisV />
+        </button>
+        {isOpen && (
+          <div className="absolute right-full top-1/2 -translate-y-[66%] bg-gray-950 rounded-md shadow-md py-2 z-50">
+            <div className="flex flex-col text-nowrap">
+              <button
+                className="px-4 py-1.5 cursor-pointer text-left text-white hover:bg-gray-800"
+                onClick={handleSetAgentConfig}
+              >
+                Set Agent Config
+              </button>
+              <button
+                className="px-4 py-1.5 cursor-pointer text-left text-white hover:bg-gray-800"
+                onClick={handleTagging}
+              >
+                Tagging
+              </button>
+              <button
+                className="px-4 py-1.5 cursor-pointer text-left text-red-300 hover:bg-gray-800"
+                onClick={handleDelete}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+const PhoneNumbers = () => {
+  const [isOverlayShow, setIsOverlayShow] = useState(false);
+  const [phoneNumbers, setPhoneNumbers] = useState<PhoneTypeRead[]>([]);
+  const [agents, setAgents] = useState<AgentTypeRead[]>([]);
+  const [isChanged, setIsChanged] = useState(false);
+  const [isSetAgentModalOpen, setIsSetAgentModalOpen] = useState(false);
+  const [isImportModalOpen, setIsImportModalOpen] = useState(false);
+  const [isSetConfigModalOpen, setIsSetConfigModalOpen] = useState(false);
+  const [selectedPhone, setSelectedPhone] = useState<PhoneTypeRead | null>(
+    null
+  );
+
+  useEffect(() => {
+    const fetchAgents = async () => {
+      try {
+        const response = await axiosInstance.get(`/agent`);
+        const data = response.data;
+        setAgents(data);
+      } catch (error) {
+        console.error(error);
+        toast.error(`Failed to fetch agents: ${error}`);
+      }
+    };
+    fetchAgents();
+  }, []);
+  useEffect(() => {
+    const fetchPhones = async () => {
+      setIsOverlayShow(true);
+      try {
+        const response = await axiosInstance.get(`/phones`);
+        const data = response.data;
+        setPhoneNumbers(data);
+      } catch (error) {
+        console.error(error);
+        toast.error(`Failed to fetch phone numbers: ${error}`);
+      } finally {
+        setIsOverlayShow(false);
+      }
+    };
+    fetchPhones();
+  }, [isChanged]);
+
+  return (
+    <Content isOverlayShown={isOverlayShow}>
+      <div className="flex flex-col gap-8 h-full">
+        <div className="flex justify-between flex-wrap gap-4">
+          <h2 className="flex items-center gap-2 text-2xl font-bold">
+            Phone Numbers
+          </h2>
+          <div className="flex gap-2 items-center">
+            <button
+              className="flex gap-2 items-center cursor-pointer bg-transparent text-sky-600 border border-sky-600 px-6 py-3 rounded-md transition-all duration-300 hover:bg-sky-600 hover:text-white"
+              onClick={() => setIsImportModalOpen(true)}
+            >
+              <FaDownload />
+              Import Number
+            </button>
+            {/* <button
+              className="flex gap-2 items-center cursor-pointer bg-sky-600 text-white px-6 py-3 rounded-md transition-all duration-300 hover:bg-sky-700"
+            >
+              <FaPlus />
+              Buy Number
+            </button> */}
+          </div>
+        </div>
+        <div className="flex flex-col justify-between h-full gap-4 rounded-lg bg-gray-900/80 overflow-x-auto">
+          <Table>
+            <thead>
+              <tr className="border-b border-gray-700">
+                <TableCell>Phone</TableCell>
+                <TableCell className="text-center">Agent</TableCell>
+                <TableCell>Region</TableCell>
+                <TableCell>Created</TableCell>
+                <TableCell />
+              </tr>
+            </thead>
+            <tbody>
+              {phoneNumbers.map((phoneNumber, index) => {
+                return (
+                  <tr key={index}>
+                    <TableCell>{phoneNumber.id}</TableCell>
+                    <TableCell className="text-center">
+                      {phoneNumber.agent_id ? (
+                        <div className="flex items-center justify-center gap-2">
+                          <div className="flex items-center rounded bg-gray-800 py-1">
+                            <FaUserAlt
+                              size={24}
+                              className="ml-2 rounded bg-gray-900 p-1.5"
+                            />
+                            <div className="text-sm px-3">
+                              {
+                                agents.find(
+                                  (agent) => agent.id === phoneNumber.agent_id
+                                )?.name
+                              }
+                            </div>
+                          </div>
+                          <button
+                            className="cursor-pointer rounded hover:bg-gray-800/50 p-2 transition-all duration-300"
+                            onClick={() => {
+                              setIsSetAgentModalOpen(true);
+                              setSelectedPhone(phoneNumber);
+                            }}
+                            disabled={isOverlayShow}
+                          >
+                            <FaEdit className="text-gray-400" />
+                          </button>
+                        </div>
+                      ) : (
+                        <button
+                          className="cursor-pointer bg-transparent text-sky-600 text-sm border border-sky-600 px-4 py-1.5 mx-auto rounded-md transition-all duration-300 hover:bg-sky-600 hover:text-white"
+                          onClick={() => {
+                            setIsSetAgentModalOpen(true);
+                            setSelectedPhone(phoneNumber);
+                          }}
+                          disabled={isOverlayShow}
+                        >
+                          Link to Agent
+                        </button>
+                      )}
+                    </TableCell>
+                    <TableCell>us-west</TableCell>
+                    <TableCell>
+                      {formatDateTime(phoneNumber.create_at)}
+                    </TableCell>
+                    <TableCell>
+                      <PhoneAction
+                        phone={phoneNumber}
+                        setIsChanged={setIsChanged}
+                        setIsOverlayShow={setIsOverlayShow}
+                        setIsSetConfigModalOpen={setIsSetConfigModalOpen}
+                        setSelectedPhoneNumber={setSelectedPhone}
+                      />
+                    </TableCell>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+          {!phoneNumbers.length && (
+            <div className="text-center m-4 p-6">
+              <div className="text-gray-400">No Active Phone Numbers Found</div>
+              {/* <div className="my-3">
+                <button
+                  className="flex gap-2 items-center cursor-pointer bg-transparent text-sky-600 border border-sky-600 px-4 py-2 mx-auto rounded-md transition-all duration-300 hover:bg-sky-600 hover:text-white"
+                >
+                  <FaPlus />
+                  Buy Number
+                </button>
+              </div> */}
+            </div>
+          )}
+        </div>
+      </div>
+      <SetAgentModal
+        agents={agents}
+        isOpen={isSetAgentModalOpen}
+        isOverlayShow={isOverlayShow}
+        selectedPhoneNumber={selectedPhone}
+        setIsChanged={setIsChanged}
+        setIsOpen={setIsSetAgentModalOpen}
+        setIsOverlayShow={setIsOverlayShow}
+        setSelectedPhoneNumber={setSelectedPhone}
+      />
+      <ImportNumberModal
+        isOpen={isImportModalOpen}
+        isOverlayShow={isOverlayShow}
+        setIsChanged={setIsChanged}
+        setIsOpen={setIsImportModalOpen}
+        setIsOverlayShow={setIsOverlayShow}
+      />
+      <SetAgentConfigModal
+        isOpen={isSetConfigModalOpen}
+        isOverlayShow={isOverlayShow}
+        selectedPhoneNumber={selectedPhone}
+        setIsChanged={setIsChanged}
+        setIsOpen={setIsSetConfigModalOpen}
+        setIsOverlayShow={setIsOverlayShow}
+        setSelectedPhoneNumber={setSelectedPhone}
+      />
+    </Content>
+  );
+};
+
+export default PhoneNumbers;
